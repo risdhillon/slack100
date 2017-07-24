ea2ff31ef04b44df1f47b9028d7cf8b1dc04ef9a
risham <t-ridhil@microsot.com>
2017-07-24 23:44:57 +0000

    changing config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  // your community or team name to display on join page.
+// my changes
+   // your community or team name to display on join page.
   community: process.env.COMMUNITY_NAME || 'PS Cloud Channel 123 Testing',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'pscstest.slack.com',
